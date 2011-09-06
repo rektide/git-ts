0834d3800db485c7012a715d85ec88afb8f94045
rektide <rektide@voodoowarez.com>
2011-09-05 22:15:25 -0400

    Dump to stdout, don't use an output-file argument.

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 #!/usr/bin/node
 
-if(process.argv.length < 3) {
-     console.log("usage: "+process.argv[1]+' output_file')
-     process.exit(1)
-}
-
 var exec= require('child_process').exec,
      fs= require('fs')
      spaces= /[\s]/,
-     pieces= [null,null],
-     outfile= process.argv[2]
+     pieces= [null,null]
 
 exec('git log -n1', function(err,stdout,stdin) {
      if(err) process.exit(2)
@@ -17,16 +11,13 @@ exec('git log -n1', function(err,stdout,stdin) {
      exec('git cat-file -p '+head,function(err,stdout_catfile,stderr_catfile) {
           if(err) process.exit(3)
           var tree= stdout.split(spaces,2)[1]
-          console.log('|tree',tree,'|')
           exec('git ls-tree -r -t -l --full-name '+tree,function(err,stdout_tree,stdin_tree) {
                if(err) process.exit(4)
-               var stream= fs.createWriteStream(outfile,{flags:'w',encoding:'utf8',mode:660})
-			
-               stream.write(head)
-               stream.write("\n")
-               stream.write(stdout_catfile)
-               stream.write("\n")
-               stream.end(stdout_tree)
+               process.stdout.write(head)
+               process.stdout.write("\n")
+               process.stdout.write(stdout_catfile)
+               process.stdout.write("\n")
+               process.stdout.end(stdout_tree)
           })
      })
 })
