7c9b9e10c592de2cfcb8b54b6da981a75050bd62
rektide <rektide@voodoowarez.com>
2011-09-06 02:51:10 -0400

    Add "install" action to git-ts, adds post-commit handler inside a git project.

--- server.js
+++ server.js
@@ -5,6 +5,41 @@ var exec= require('child_process').exec,
      spaces= /[\s]/,
      pieces= [null,null]
 
+// this first section, install, builds post-commit files.
+if(process.argv[2] == 'install') {
+	var path= require('path'),
+		cwd= process.cwd()
+		dir= cwd
+	while(dir.length > 1) {
+		var trial= null,
+			git_dir= dir+"/.git/hooks"
+		try {
+			trial= fs.statSync(git_dir)
+		} catch(ex) {
+		}
+		if(trial && trial.isDirectory) {
+			dir= git_dir
+			break;
+		}
+		dir= path.dirname(dir)
+	}
+	if(dir.length <= 1) {
+		process.exit(1)
+	}
+
+	var dest= dir+"/post-commit"
+	try{
+		fs.statSync(dest)
+		dest= dest+".git-js-default"
+	}catch(ex){
+	}
+
+	fs.writeFileSync(dest,"#!/bin/sh\ngit-ts-publish")
+	process.exit()
+}
+
+process.exit(99)
+
 exec('git log -n1', function(err,stdout,stdin) {
      if(err) process.exit(2)
      var head= stdout.split(spaces,2)[1]
